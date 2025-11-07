3167050557d8388c4075db81c6a455061342f934
Otega Otite <otiteotega@gmail.com>
2025-11-07 10:39:23 +0100

    Print to console

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Create a Script to print "JavaScript is fun" to the console.
+ * You are not allowed to use var
+ * you are only to write your script in one line. 
+ */
+
+// Script to print a text
+console.log('JavaScript is fun')
+
