9b4151070ba6cd6c0a2710f80f88b2f544e94763
Otega Otite <otiteotega@gmail.com>
2025-11-07 10:51:17 +0100

    print on three lines

---
+++ index2.js
@@ -0,0 +1,6 @@
+/**
+ * Create a Script to print the following: I love JavaScript, This is my heritage
+ * I am from Nigeria on a separate line
+ */
+
+console.log("I love JavaScript\nThis is my Heritage\nI am from Nigeria")
